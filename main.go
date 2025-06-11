4c2a9db20afda5f52b89bbc87b724f98c755f0d7
Mike Rybka <merybka@gmail.com>
2025-06-10 21:37:17 -0400

    Install go directly from hosted archive

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	v := version()
+	filename := fmt.Sprintf("%s.%s-%s.tar.gz", v, runtime.GOOS, runtime.GOARCH)
+	u := fmt.Sprintf("https://go.dev/dl/%s", filename)
+	res, err := http.Get(u)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	gzr, err := gzip.NewReader(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer gzr.Close()
+	tr := tar.NewReader(gzr)
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break // End of archive
+		}
+		if err != nil {
+			panic(err)
+		}
+		if header.Name == "go/bin/go" {
+			f, err := os.Create("/usr/bin/go")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			_, err = io.Copy(f, tr)
+			if err != nil {
+				panic(err)
+			}
+		}
+		if header.Name == "go/bin/gofmt" {
+			f, err := os.Create("/usr/bin/gofmt")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			_, err = io.Copy(f, tr)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func version() string {
+	res, err := http.Get("https://go.dev/VERSION?m=text")
+	if err != nil {
+		panic(err)
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(b), "\n")
+	return lines[0]
+}
